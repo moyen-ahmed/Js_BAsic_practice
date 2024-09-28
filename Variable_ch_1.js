6c4348c72535485ed21c6e0939a343ed8e1f9d61
emas_tech <estyakahmefmoyen@gmail.com>
2024-09-28 22:53:34 +0600

    lec 1

---
+++ Variable_ch_1.js
@@ -0,0 +1,34 @@
+//Variables................................
+fname="Ishtiak";
+fName="Ishtiak";//case  sensative
+//only letters,digits,_dollar sign are allowed
+//letters ,_,dollar are allowed in 1st carecter
+console.log(fname);
+console.log(fName);
+age=45;
+console.log(age);
+//var(not use),let,Const(no change fixed valu) use 
+let lastname="Moyen";
+console.log(lastname);
+const mname="Rana";
+console.log(mname);
+//Data Tpye...........
+fname="Ishtiak";
+typeof fname; 
+//obhject,.........
+const student={
+    fname:"Ishtiak Ahmed Moyen",
+    age:20,
+    University:"North South Universty",
+    isPass:true,
+
+};
+console.log(student);
+//Practicce
+const pen_product={
+    titte:"Ball pen",
+    rating:4,
+    offer:5,
+    price:240,
+};
+console.log(pen_product)
