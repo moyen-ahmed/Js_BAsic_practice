77062cf199204849bd6fbf303f93fa70507792a9
emas_tech <estyakahmefmoyen@gmail.com>
2024-10-01 02:22:45 +0600

    lec3

---
+++ DOM_Part_1_Lec_3.js
@@ -0,0 +1,7 @@
+//Docment Object Model 
+console.log("Hi");
+alert("Gi");
+//Window glonbal Object
+console.log(document)
+console.log(document.body )
+//Use Javascript we can change daynamic change ..without change html and css code.
